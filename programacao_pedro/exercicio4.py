bbe500388bc1e24f38a7d5c197b6c6b0f4f7f2db
pedrojgcoelho <124636758+pedrojgcoelho@users.noreply.github.com>
2024-02-28 12:22:19 -0300

    Add files via upload

---
+++ programacao_pedro/exercicio4.py
@@ -0,0 +1,15 @@
+valor1 = input ("Digite o valor 1: ")
+valor2 = input ("Digite o valor 2: ")
+
+
+if valor1 >=  valor2 :
+    print(f"{valor1=} é maior ou igual que o {valor2=}")
+elif valor1 < valor2:
+    print (f"{valor1=} é menor que o {valor2=}")
+
+
+
+#if valor1 > valor2:
+#    print("O primeiro valor é maior.")
+#elif valor1 < valor2:
+#    print("O segundo valor é maior.")
